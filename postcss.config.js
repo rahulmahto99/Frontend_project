99156dce834277ce5a6acd454c6e605bf424e4a2
Rahul Mahto <Rahulmahto837@gmail.com>
2024-06-25 23:15:59 +0530

    folder

---
+++ postcss.config.js
@@ -0,0 +1,10 @@
+// postcss.config.js
+
+module.exports = {
+    plugins: [
+      require('tailwindcss'),
+      require('autoprefixer'),
+      // Any other PostCSS plugins as needed
+    ],
+  };
+  
