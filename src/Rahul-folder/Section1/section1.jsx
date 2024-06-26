8ce1aec64b8b300fea403e2070e11affa294b1ce
Rahul Mahto <Rahulmahto837@gmail.com>
2024-06-26 21:47:59 +0530

    Your commit message

--- src/Rahul-folder/Section1/section1.jsx
+++ src/Rahul-folder/Section1/section1.jsx
@@ -1,11 +1,17 @@
 
-
 function Section1() {
   return (
+    <>
     <div className="bg-gray-200 p-4 rounded-md shadow-md">
       <h1 className="text-xl font-bold text-red-900 underline underline-offset-4 ">Hello, Tailwind CSS!</h1>
       <p className="text-gray-700 mt-2">This is a Tailwind CSS example.</p>
     </div>
+    <div className="bg-gray-200 p-4 rounded-md shadow-md">
+      <h1 className="text-xl font-bold text-red-900 underline underline-offset-4 ">Hello, Tailwind CSS!</h1>
+      <p className="text-gray-700 mt-2">This is a Tailwind CSS example.</p>
+    </div>
+     </>
+     
   );
 }
 
