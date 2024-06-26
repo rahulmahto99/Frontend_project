1272513e334cdf415f4184bcefc5effe56adfd4c
Ankur Kumar Singh <148796895+Akgitter@users.noreply.github.com>
2024-06-26 21:41:35 +0530

    Test

--- src/Ankur-folder/Section1/section1.jsx
+++ src/Ankur-folder/Section1/section1.jsx
@@ -2,9 +2,10 @@ import React from 'react'
 
 function Section1() {
   return (
-    <div className="bg-gray-200 p-4 rounded-md shadow-md">
+    <div className="page bg-blend-color">
+        <div className="container"></div>
       <h1 className="text-xl font-bold text-blue-600 underline underline-offset-4 ">Hello, Tailwind CSS!</h1>
-      <p className="text-gray-700 mt-2">This is a Tailwind CSS example.</p>
+      <p className="text-gray-700 mt-2">This is a Tailwind CSS example Ankur.</p>
     </div>
   );
 }
