31d6f6cf165b3d57553ec11c42278673d75eedd3
ritik kumar <139105262+kumarritik001@users.noreply.github.com>
2024-06-27 20:12:11 +0530

    itna hi hua bhai

--- src/Ritik-folder/Section1/Section1.jsx
+++ src/Ritik-folder/Section1/Section1.jsx
@@ -2,17 +2,17 @@ import React from 'react'
 
 function Section1() {
   return (
-    <div className="bg-gray-200 p-4 rounded-md shadow-md">
+    /*<div className="bg-gray-200 p-4 rounded-md shadow-md">
       {/* <h1 className="text-xl font-bold text-blue-600 underline underline-offset-4 ">Hello, Tailwind CSS!</h1>
-      <p className="text-gray-700 mt-2">This is a Tailwind CSS example.</p> */}
-      <div class="flex flex-row bg-white p-4"> 
+      <p className="text-gray-700 mt-2">This is a Tailwind CSS example.</p> */
+    /*  <div class="flex flex-row bg-white p-4"> 
           <div1>
            <img src='https://demo2.pavothemes.com/insul/wp-content/uploads/2024/04/h1-faq.jpg'  class="min-h-80vh  rounded-3xl p-4 min-w-50vh "/> 
           </div1>
           <div2 class=" min-h-50vh min-w-100vh">
           <i class="fa-solid fa-circle"/>PRICING FCQS
 
-            <h1 >
+            <h1 class ="text-4xl font-bold" >
             Frequently asked questions
             </h1>
            
@@ -21,69 +21,143 @@ function Section1() {
        
       <div class="w-full max-w-lg bg-white shadow-lg rounded-lg">
 
-        {/*button1*/}
-              <div class="border-b">
-                <button id="dropdownButton1" class="w-full text-left p-4 text-xl font-semibold text-blue-600 flex justify-between items-center">
-                  <span>01. What is it that makes you stand out in the industry?</span>
-                  <svg id="dropdownIcon1" class="w-6 h-6 transform transition-transform duration-200" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                  <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 9l-7 7-7-7" />
-                  </svg>
-                </button>
-              </div>
-            <div id="dropdownContent1" class="p-4 text-gray-700 hidden">
-              <p>Of course, I'd be happy to help you with a business review. However, could you please provide more specific details about the business you’d like to review? Are you looking for a review of a particular company’s.</p>
-            </div>
-        
-        {/*button2*/}
-            <div class="border-b">
-              <button id="dropdownButton2" class="w-full text-left p-4 text-xl font-semibold text-blue-600 flex justify-between items-center">
-                <span>02. How do you ensure customer satisfaction?</span>
-                <svg id="dropdownIcon2" class="w-6 h-6 transform transition-transform duration-200" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 9l-7 7-7-7" />
-                </svg>
-            </button>
-            </div>
-            <div id="dropdownContent2" class="p-4 text-gray-700 hidden">
-               We prioritize customer feedback and implement quality assurance processes to meet and exceed customer expectations.
-            </div>
-        
+        {/*button1*/
+   //           <div class="border-b">
+   //             <button id="dropdownButton1" class="w-full text-left p-4 text-xl font-semibold text-blue-600 flex justify-between items-center">
+   //               <span>01. What is it that makes you stand out in the industry?</span>
+   //               <svg id="dropdownIcon1" class="w-6 h-6 transform transition-transform duration-200" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+   //               <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 9l-7 7-7-7" />
+   //               </svg>
+   //             </button>
+   //           </div>
+   //         <div id="dropdownContent1" class="p-4 text-gray-700 hidden">
+   //           <p>Of course, I'd be happy to help you with a business review. However, could you please provide more specific details about the business you’d like to review? Are you looking for a review of a particular company’s.</p>
+   //         </div>
+   //     
+   //     {/*button2*/}
+   //         <div class="border-b">
+   //           <button id="dropdownButton2" class="w-full text-left p-4 text-xl font-semibold text-blue-600 flex justify-between items-center">
+   //             <span>02. How do you ensure customer satisfaction?</span>
+   //             <svg id="dropdownIcon2" class="w-6 h-6 transform transition-transform duration-200" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+   //             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 9l-7 7-7-7" />
+   //             </svg>
+   //         </button>
+   //         </div>
+   //         <div id="dropdownContent2" class="p-4 text-gray-700 hidden">
+   //            We prioritize customer feedback and implement quality assurance processes to meet and exceed customer expectations.
+   //         </div>
+   //     
+//
+   //     {/*button3*/}
+   //         <div class="border-b">
+   //           <button id="dropdownButton3" class="w-full text-left p-4 text-xl font-semibold text-blue-600 flex justify-between items-center">
+   //             <span>03. What are your main products or services?</span>
+   //             <svg id="dropdownIcon3" class="w-6 h-6 transform transition-transform duration-200" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+   //             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 9l-7 7-7-7" />
+   //             </svg>
+   //           </button>
+   //         </div>
+   //         <div id="dropdownContent3" class="p-4 text-gray-700 hidden">
+   //           Our main products include software solutions, IT consulting, and cloud services designed to help businesses succeed.
+   //         </div>
+//
+//
+//
+   //     {/*button4*/}
+   //         <div>
+   //           <button id="dropdownButton4" class="w-full text-left p-4 text-xl font-semibold text-blue-600 flex justify-between items-center">
+   //             <span>04. Can you provide examples of your successful projects?</span>
+   //             <svg id="dropdownIcon4" class="w-6 h-6 transform transition-transform duration-200" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+   //             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 9l-7 7-7-7" />
+   //             </svg>
+   //           </button>
+   //         </div>
+   //         <div id="dropdownContent4" class="p-4 text-gray-700 hidden">
+   //           Yes, we have completed numerous projects, including a recent implementation of a custom ERP system for a leading manufacturing firm.
+   //           </div>
+         //   </div>
+  
 
-        {/*button3*/}
-            <div class="border-b">
-              <button id="dropdownButton3" class="w-full text-left p-4 text-xl font-semibold text-blue-600 flex justify-between items-center">
-                <span>03. What are your main products or services?</span>
-                <svg id="dropdownIcon3" class="w-6 h-6 transform transition-transform duration-200" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 9l-7 7-7-7" />
-                </svg>
-              </button>
-            </div>
-            <div id="dropdownContent3" class="p-4 text-gray-700 hidden">
-              Our main products include software solutions, IT consulting, and cloud services designed to help businesses succeed.
-            </div>
 
-
-
-        {/*button4*/}
-            <div>
-              <button id="dropdownButton4" class="w-full text-left p-4 text-xl font-semibold text-blue-600 flex justify-between items-center">
-                <span>04. Can you provide examples of your successful projects?</span>
-                <svg id="dropdownIcon4" class="w-6 h-6 transform transition-transform duration-200" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 9l-7 7-7-7" />
-                </svg>
-              </button>
-            </div>
-            <div id="dropdownContent4" class="p-4 text-gray-700 hidden">
-              Yes, we have completed numerous projects, including a recent implementation of a custom ERP system for a leading manufacturing firm.
-            </div>
-      </div>
-
-
-
+         // </div2>*/
 
             
-          </div2>
-      </div>
+     /* </div>*/
+    /*</div>*/
+
+
+
+<>
+<div class="w-full bg-white">
+<div class="max-w-6xl mx-auto py-12">
+        <div class="bg-white rounded-lg shadow-md overflow-hidden">
+            <div class="flex flex-col md:flex-row items-center p-6">
+                <img src="https://demo2.pavothemes.com/insul/wp-content/uploads/2024/04/h1-faq.jpg" alt="Team High Five" class="w-1/2 rounded-lg"/>
+                <div class="w-1/2 pl-6">
+                    <h3 class="text-blue-600 italic font-bold "> 🧿PRICING FAQS</h3>
+                    <h2 class="text-3xl font-bold mb-4">Frequently asked questions</h2>
+                    <p class="text-gray-600 mb-6">
+                        There are questions that you might ask about our pricing plans and service. Contact us for more info on page <br></br>
+                        <a href="#" class="text-blue-600">Contact us</a>
+                    </p>
+                    <div>
+                        <div class="mb-4">
+                            <button class="flex justify-between w-full p-4 text-left text-xl font-medium text-gray-900 bg-gray-100 rounded-lg hover:bg-gray-200 focus:outline-none focus:ring focus:ring-gray-500">
+                                01. What is it that makes you stand out in the industry?
+                                <svg class="w-5 h-5 ml-2 transform rotate-0 transition-transform" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+                                    <path fill-rule="evenodd" d="M5.23 7.21a.75.75 0 011.06 0L10 10.94l3.71-3.73a.75.75 0 111.06 1.06l-4 4a.75.75 0 01-1.06 0l-4-4a.75.75 0 010-1.06z" clip-rule="evenodd"></path>
+                                </svg>
+                            </button>
+                            <div class="p-4 text-gray-700 bg-gray-50 rounded-lg hidden">
+                                Of course, I’d be happy to help you with a business review. However, could you please provide more specific details about the business you’d like to review? Are you looking for a review of a particular company’s.
+                            </div>
+                        </div>
+                        <div class="mb-4">
+                            <button class="flex justify-between w-full p-4 text-left text-xl font-medium text-gray-900 bg-gray-100 rounded-lg hover:bg-gray-200 focus:outline-none focus:ring focus:ring-gray-500">
+                                02. In which countries can I find your company?
+                                <svg class="w-5 h-5 ml-2 transform rotate-0 transition-transform" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+                                    <path fill-rule="evenodd" d="M5.23 7.21a.75.75 0 011.06 0L10 10.94l3.71-3.73a.75.75 0 111.06 1.06l-4 4a.75.75 0 01-1.06 0l-4-4a.75.75 0 010-1.06z" clip-rule="evenodd"></path>
+                                </svg>
+                            </button>
+                            <div class="p-4 text-gray-700 bg-gray-50 rounded-lg hidden">
+                            Of course, I’d be happy to help you with a business review. However, could you please provide more specific details about the business you’d like to review? Are you looking for a review of a particular company’s.
+                            </div>
+                        </div>
+                        <div class="mb-4">
+                            <button class="flex justify-between w-full p-4 text-left text-xl font-medium text-gray-900 bg-gray-100 rounded-lg hover:bg-gray-200 focus:outline-none focus:ring focus:ring-gray-500">
+                                03. Which industries does your company provide its services?
+                                <svg class="w-5 h-5 ml-2 transform rotate-0 transition-transform" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+                                    <path fill-rule="evenodd" d="M5.23 7.21a.75.75 0 011.06 0L10 10.94l3.71-3.73a.75.75 0 111.06 1.06l-4 4a.75.75 0 01-1.06 0l-4-4a.75.75 0 010-1.06z" clip-rule="evenodd"></path>
+                                </svg>
+                            </button>
+                            <div class="p-4 text-gray-700 bg-gray-50 rounded-lg hidden">
+                            Of course, I’d be happy to help you with a business review. However, could you please provide more specific details about the business you’d like to review? Are you looking for a review of a particular company’s.
+                            </div>
+                        </div>
+                        <div class="mb-4">
+                            <button class="flex justify-between w-full p-4 text-left text-xl font-medium text-gray-900 bg-gray-100 rounded-lg hover:bg-gray-200 focus:outline-none focus:ring focus:ring-gray-500">
+                                04. What is your service expertise?
+                                <svg class="w-5 h-5 ml-2 transform rotate-0 transition-transform" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+                                    <path fill-rule="evenodd" d="M5.23 7.21a.75.75 0 011.06 0L10 10.94l3.71-3.73a.75.75 0 111.06 1.06l-4 4a.75.75 0 01-1.06 0l-4-4a.75.75 0 010-1.06z" clip-rule="evenodd"></path>
+                                </svg>
+                            </button>
+                            <div class="p-4 text-gray-700 bg-gray-50 rounded-lg hidden">
+                            Of course, I’d be happy to help you with a business review. However, could you please provide more specific details about the business you’d like to review? Are you looking for a review of a particular company’s.
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
+
+</div>
+ 
+
+
+</>
+
+
   );
 }
 
