2a943b6f51ccca548a7a50599cbc6408f9ef7d2a
Dimpymittal22 <mdimpy223@gmail.com>
2024-06-30 16:38:20 +0530

    footer

--- src/Footer/Footer.jsx
+++ src/Footer/Footer.jsx
@@ -2,7 +2,14 @@ import React from 'react'
 
 function Footer() {
   return (
-    <div>Footer</div>
+    <div className="container block w-full max-w-full px-4">
+      <div className="header text-center">
+      <h3 className="title text-[#0b0335] text-xl">
+        <span className="">Latest Posts</span>
+      </h3>
+      </div>
+    </div>
+    
   )
 }
 
