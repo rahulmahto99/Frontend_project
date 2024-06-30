b62e2d5330b85109a2c760ba4b8bfccfc08afb7a
Rahul Mahto <Rahulmahto837@gmail.com>
2024-06-30 09:16:37 +0530

    Create folder for dimpy

--- src/Header/Header.jsx
+++ src/Header/Header.jsx
@@ -12,33 +12,39 @@ const Header = () => {
   return (
     // Header Notification Start Here
     <div className="w-full ">
-      <div className="notification-header bg-[url('https://new-ella-demo.myshopify.com/cdn/shop/files/bg_announcement_countdow_2048x.jpg?v=1640253389')] bg-cover bg-no-repeat bg-center min-h-32 flex justify-between items-center px-5 w-full">
-        <div></div>
-        <div className="flex flex-col items-center mx-auto">
-          <h1 className="text-xs font-bold text-white">Limited-time Offer!</h1>
-          <Countdown />
-        </div>
-        <div>
-          <RxCross1 color="white" />
+      <div className="notification-header bg-[url('https://new-ella-demo.myshopify.com/cdn/shop/files/bg_announcement_countdow_2048x.jpg?v=1640253389')] bg-cover bg-no-repeat bg-center min-h-32 px-5 w-full">
+        <div className="flex justify-between max-w-[1200px] mx-auto  min-h-32  items-center">
+          <div>{/* first box */}</div>
+          <div className="flex flex-col items-center ">
+            <h1 className="text-xs font-bold text-white">
+              Limited-time Offer!
+            </h1>
+            <Countdown />
+          </div>
+          <div>
+            <RxCross1 color="white" />
+          </div>
         </div>
       </div>
-      <div className=" Header-top max-w-[1200px] mx-auto bg-[#F7F8FA]  py-2 flex justify-between">
-        <div className="child1 flex gap-3">
-          <div className="phone text-xs flex items-center font-normal text-[#06142e]">
-            <FaPhoneAlt size={16} />
-            <span className="pl-2">Toll-free: (091) 123-4567</span>
+      <div className=" Header-top bg-[#F7F8FA]  py-3 ">
+        <div className="max-w-[1200px] mx-auto flex justify-between  ">
+          <div className="child1 flex gap-3">
+            <div className="phone text-xs flex items-center font-normal text-[#06142e]">
+              <FaPhoneAlt size={16} />
+              <span className="pl-2">Toll-free: (091) 123-4567</span>
+            </div>
+            <div className="email text-xs flex items-center font-normal text-[#06142e]">
+              <CiMail size={16} />
+              <span className="pl-2">email@domain.com</span>
+            </div>
           </div>
-          <div className="email text-xs flex items-center font-normal text-[#06142e]">
-            <CiMail size={16} />
-            <span className="pl-2">email@domain.com</span>
+          <div className="child2 text-xs font-bold text-[#06142e]">
+            <h1>Free Shipping Over $99*</h1>
           </div>
-        </div>
-        <div className="child2 text-xs font-bold text-[#06142e]">
-          <h1>Free Shipping Over $99*</h1>
-        </div>
-        <div className="child3 flex gap-3">
-          <h1 className="text-xs font-normal text-[#06142e]">Theme FAQs</h1>
-          <h1 className="text-xs font-normal text-[#06142e]">Need Help?</h1>
+          <div className="child3 flex gap-3">
+            <h1 className="text-xs font-normal text-[#06142e]">Theme FAQs</h1>
+            <h1 className="text-xs font-normal text-[#06142e]">Need Help?</h1>
+          </div>
         </div>
       </div>
       <div className=" Main-header max-w-[1200px] mx-auto flex py-4  bg-white  max-auto items-center">
@@ -63,25 +69,25 @@ const Header = () => {
         </div>
         <div className="flex basis-4/12 justify-between">
           <div className="cart flex items-center">
-            <CiShoppingCart  size={30} style={{ fontWeight: 'bold' }}/>
+            <CiShoppingCart size={30} style={{ fontWeight: "bold" }} />
             <span className="text-base font-medium text=[#06142e]">Cart</span>
           </div>
           <div className="wishlist  flex items-center">
-            <CiHeart  size={25} style={{ fontWeight: 'bold' }} />
-            <span className="text-base font-medium text=[#06142e]">Wishlist</span>
+            <CiHeart size={25} style={{ fontWeight: "bold" }} />
+            <span className="text-base font-medium text=[#06142e]">
+              Wishlist
+            </span>
           </div>
           <div className="signin flex  items-center">
-            <FaRegUser  size={25} style={{ fontWeight: 'bold' }} />
-            <span  className="text-base font-medium text=[#06142e]">Sign in Or Register</span>
+            <FaRegUser size={25} style={{ fontWeight: "bold" }} />
+            <span className="text-base font-medium text=[#06142e]">
+              Sign in Or Register
+            </span>
           </div>
         </div>
       </div>
     </div>
     // Header Notification End Here
-
-    // Main Header Start Here
-
-    // Main Header End Here
   );
 };
 
--- src/ProductCategories/Product-categories.jsx
+++ src/ProductCategories/Product-categories.jsx
@@ -4,8 +4,8 @@ function ProductCategories() {
   return (
     <div className="flex gap-6 px-5 py-10 max-w-[1200px] mx-auto">
       <div className=" basis-4/12 overflow-hidden">
-        <div className="basis-4/12  bg-[url('https://new-ella-demo.myshopify.com/cdn/shop/files/h9s1-custom-image-banner-1_370x.jpg?v=1639976027')] h-screen bg-no-repeat bg-cover  pt-10 pl-8 hover:scale-105 hover:transition-all">
-          <h1 className="text-xl flex items-center gap-1 text-[#0b0335] font-extrabold">
+        <div className="basis-4/12  bg-[url('https://new-ella-demo.myshopify.com/cdn/shop/files/h9s1-custom-image-banner-1_370x.jpg?v=1639976027')] h-screen-1/2 bg-no-repeat bg-cover  pt-10 pl-8 hover:scale-105 hover:transition-all">
+          <h1 className="text-xl flex items-center gap-1 text-[rgb(11,3,53)] font-extrabold">
             SMARTPHONES <FaAngleDown />
           </h1>
           <h1 className="text-base font-normal text-[#0b0335]">from $899*</h1>
@@ -13,7 +13,7 @@ function ProductCategories() {
       </div>
       <div className=" flex flex-col basis-4/12">
         <div className=" basis-3/6 overflow-hidden">
-          <div className=" bg-[url('https://new-ella-demo.myshopify.com/cdn/shop/files/h9s1-custom-image-banner-2_370x.jpg?v=16399760277')] h-3/6 bg-no-repeat bg-cover pb-52   pt-10 pl-8 hover:scale-105 hover:transition-all">
+          <div className=" bg-[url('https://new-ella-demo.myshopify.com/cdn/shop/files/h9s1-custom-image-banner-2_370x.jpg?v=16399760277')] h-2/4	 bg-no-repeat bg-cover pb-52   pt-10 pl-8 hover:scale-105 hover:transition-all">
             <h1 className="text-xl flex items-center gap-1 text-[#0b0335] font-extrabold">
               Tablets <FaAngleDown />
             </h1>
@@ -21,7 +21,7 @@ function ProductCategories() {
           </div>
         </div>
         <div className=" basis-3/6 overflow-hidden">
-          <div className="bg-[url('https://new-ella-demo.myshopify.com/cdn/shop/files/h9s1-custom-image-banner-4_370x.jpg?v=1639976027')] h-3/6 bg-no-repeat bg-cover  pt-10 pl-8 hover:scale-105 p-52 hover:transition-all">
+          <div className="bg-[url('https://new-ella-demo.myshopify.com/cdn/shop/files/h9s1-custom-image-banner-4_370x.jpg?v=1639976027')] h-2/4 bg-no-repeat bg-cover  pt-10 pl-8 hover:scale-105 p-52 hover:transition-all">
             <h1 className="text-xl flex items-center gap-1 text-[#0b0335] font-extrabold">
               Headphones <FaAngleDown />
             </h1>
